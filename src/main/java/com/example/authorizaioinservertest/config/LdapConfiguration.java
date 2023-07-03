d7de42e1d090ba42a754a2e83a208909de757fd5
FIT1037 <ken.sp.chiu@fit-foxconn.com>
2023-07-03 10:50:26 +0800

    DM-128 Datahub使用AD進行使用者認證。在OIDC中加入名稱與email。

---
+++ src/main/java/com/example/authorizaioinservertest/config/LdapConfiguration.java
@@ -0,0 +1,2 @@
+package com.example.authorizaioinservertest.config;public class LdapConfiguration {
+}
---
+++ src/main/java/com/example/authorizaioinservertest/service/UserService.java
@@ -0,0 +1,2 @@
+package com.example.authorizaioinservertest.service;public class UserService {
+}
